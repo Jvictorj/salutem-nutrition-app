b7c52d0e2b0fec9ffe632962be4690f4d4ace087
JoaoV <joaovitorgsdeveloper@gmail.com>
2025-12-15 14:22:10 -0300

    chore: adicionar template do environment.prod com apis

--- src/environments/environment.prod.example.ts
+++ src/environments/environment.prod.example.ts
@@ -1,13 +1,28 @@
 export const environment = {
-  production: false,
+  production: true,
+
   firebaseConfig: {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: ""
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+    measurementId: '',
+  },
+
+  apis: {
+    wger: {
+      url: 'https://wger.de/api/v2'
+    },
+    spoonacular: {
+      url: 'https://api.spoonacular.com/recipes/complexSearch',
+      key: ''
+    },
+    youtube: {
+      url: 'https://www.googleapis.com/youtube/v3/search',
+      key: ''
+    }
   }
 };
